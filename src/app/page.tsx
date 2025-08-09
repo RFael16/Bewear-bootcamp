7170ab18ed2a7eb487fce7f8df16e2dc87582030
Rfael16 <frcrash7122@gmail.com>
2025-08-09 13:11:43 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import { Header } from "@/components/common/header";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-const Home = () => {
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
+      <div className="px5 space-y-6">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <Image
+          src="/banner-02.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 };
